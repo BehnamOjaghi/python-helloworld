2b714993ba2aefebffd58280c4467e422f0a0f42
BehnamOjaghi <behnam.ojaghi2@gmail.com>
2021-01-20 05:21:30 +0330

    comment

--- beh.py
+++ beh.py
@@ -3,7 +3,7 @@ print('hi behnam')
 print('================================================')
 
 import os
-path='/content/drive/MyDrive/Colab Notebooks/DataSet/sp'
-for root, dirs, files in os.walk(path):
-    for filename in files:
-        print(filename)
+# path='/content/drive/MyDrive/Colab Notebooks/DataSet/sp'
+# for root, dirs, files in os.walk(path):
+#     for filename in files:
+#         print(filename)
